a420cee3781ccd6ef6f1986dff9681b6e1797aff
Giosue1994 <giosuelentini1994@outlook.com>
2020-05-25 18:57:11 +0200

    esercizio email

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 // lista delle email
-var email = ['calamaro39@gmail.com', 'cammello98@gmail.com', 'gesu33@gmail.com', 'sternocleidomastoideo11@gmail.com', 'anubidc@gmail.com'];
+var email = ['calamaro39@gmail.com', 'sternocleidomastoideo11@gmail.com', 'cammello98@gmail.com', 'gesu33@gmail.com', 'anubidc@gmail.com'];
 // presenza della email nella lista
 var sternocleidomastoideo = false;
 // email da cercare
 var cercaEmail = 'sternocleidomastoideo11@gmail.com';
 
-var inserisci =document.getElementById('inserisci-email');
-var invia =document.getElementById('invia');
+var tuaEmail = document.getElementById('tua-email');
+var emailUtente = document.getElementById('email-utente');
+var inserisci = prompt('inserisci la tua email');
+tuaEmail.innerHTML = 'La tua email è: ';
 
-inserisci.innerHTML = 'Inserisci Email';
 
-
+// l'email inserita deve essere uguale a quella specificata sopra
+inserisci == cercaEmail;
 
 //controllo se la mia email è nella lista
 for ( var i = 0; i < email.length; i++ ) {
-  if(email[i] === cercaEmail ) {
+  if(email[i] === inserisci ) {
     sternocleidomastoideo = true;
   }
-  console.log(email[i]);
 }
 
-invia.addEventListener('ckick',
-function() {
-
-  }
-);
+// se l'email inserita nel prompt è vera compare sullo schermo altrimente esce fuori un errore
+if (sternocleidomastoideo) {
+  emailUtente.innerHTML = inserisci;
+}else {
+  tuaEmail.innerHTML = 'Email non corretta';
+  alert('ERRORE');
+}
