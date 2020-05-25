ae72698c25d540973e484bddea304894f4d5fe9b
Giosue1994 <giosuelentini1994@outlook.com>
2020-05-25 19:58:35 +0200

    gioco dei dadi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ________ ESERCIZIO EMAIL ________
 // lista delle email
 var email = ['calamaro39@gmail.com', 'sternocleidomastoideo11@gmail.com', 'cammello98@gmail.com', 'gesu33@gmail.com', 'anubidc@gmail.com'];
 // presenza della email nella lista
@@ -27,4 +28,32 @@ if (sternocleidomastoideo) {
 }else {
   tuaEmail.innerHTML = 'Email non corretta';
   alert('ERRORE');
+}
+
+
+// ________ ESERCIZIO DADI ________
+var gioco = document.getElementById('gioco').innerHTML = 'Gioco dei dadi';
+var risultato = document.getElementById('risultato');
+
+// calcolo casuale dei numeri da parte del computer
+var computer = Math.floor(Math.random() * 6) + 1;
+
+// l'utente che inserisce il numero
+var player = parseInt(prompt('Scegli un numero da 1 a 6'));
+
+// vittoria / sconfitta
+if (player > computer) {
+  alert('HAI VINTO');
+  risultato.innerHTML = 'VITTORIA';
+}
+else if (player == computer) {
+  alert('PAREGGIO');
+  risultato.innerHTML = 'PAREGGIO';
+}
+else if (isNaN(player)) {
+  alert('Inserire solo numeri');
+}
+else {
+  alert('HAI PERSO');
+  risultato.innerHTML = 'SCONFITTA';
 }
